5f4e08f136218e6791125fe5f060bca01debb0b7
윤성호 <youn2029@naver.com>
2019-02-07 20:42:01 +0900

    테이블 뷰 컨트롤러 설정

---
+++ MyMovieChart/ListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ListViewController.swift
+//  MyMovieChart
+//
+//  Created by 윤성호 on 07/02/2019.
+//  Copyright © 2019 윤성호. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UITableViewController {
+    
+    
+}
