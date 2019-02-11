1f7d4d63f0edb1f8e51c93657e2b894d3be381c5
윤성호 <youn2029@naver.com>
2019-02-12 01:06:26 +0900

    imageView를 이용한 thumbnail 추가

--- MyMovieChart/ListViewController.swift
+++ MyMovieChart/ListViewController.swift
@@ -12,9 +12,9 @@ class ListViewController: UITableViewController {
     
     // 튜플 아이템으로 구성된 데이터 세트
     var dataset = [
-        ("다크나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95),
-        ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31),
-        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19)
+        ("다크나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95, "darknight.jpg"),
+        ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31, "rain.jpg"),
+        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19, "secret.jpg")
     ]
     
     // 테이블 뷰를 구성할 리스트 데이터
@@ -22,7 +22,7 @@ class ListViewController: UITableViewController {
         
         var datalist = [MovieVO]()
         
-        for (title, desc, opendate, rating) in self.dataset {
+        for (title, desc, opendate, rating, thumbnail) in self.dataset {
             
             let mvo = MovieVO()
             
@@ -30,6 +30,7 @@ class ListViewController: UITableViewController {
             mvo.description = desc
             mvo.opendate = opendate
             mvo.rating = rating
+            mvo.thumbnail = thumbnail
             
             datalist.append(mvo)
         }
@@ -67,6 +68,8 @@ class ListViewController: UITableViewController {
         cell.desc?.text = row.description
         cell.opendate?.text = row.opendate
         cell.rating?.text = "\(row.rating!)"
+        
+        cell.thumbnail.image = UIImage(named: row.thumbnail!)
         
         /// ============ 커스텀 style (태그속성) ============
 //        // 테이블 셀 객체를 직접 생성하는 대신 큐로부터 가져온다
--- MyMovieChart/MovieCell.swift
+++ MyMovieChart/MovieCell.swift
@@ -10,9 +10,10 @@ import UIKit
 
 class MovieCell: UITableViewCell {
     
-    @IBOutlet var title: UILabel!       // 영화제목
-    @IBOutlet var desc: UILabel!        // 영화 요약
-    @IBOutlet var opendate: UILabel!    // 개봉일
-    @IBOutlet var rating: UILabel!      // 평점
+    @IBOutlet var thumbnail: UIImageView!   // 섬네일 이미지
+    @IBOutlet var title: UILabel!           // 영화제목
+    @IBOutlet var desc: UILabel!            // 영화 요약
+    @IBOutlet var opendate: UILabel!        // 개봉일
+    @IBOutlet var rating: UILabel!          // 평점
     
 }
