69c365697ca4ea9554e792ad0ea9cbf859a24a53
윤성호 <youn2029@naver.com>
2019-03-03 22:14:10 +0900

    Activity Indicator VIew를 이용하여 웹 뷰 로딩 를 추가

--- MyMovieChart/DetailVIewController.swift
+++ MyMovieChart/DetailVIewController.swift
@@ -9,14 +9,53 @@
 import UIKit
 import WebKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, WKNavigationDelegate{
     
     @IBOutlet var wv: WKWebView!    // 웹 뷰
+    @IBOutlet var spinner: UIActivityIndicatorView! // 인디케이터 뷰
     
     // 목록 화면에서 전달하는 영화 정보를 받는 변수
     var mvo : MovieVO!
     
+    // Called when web content begins to load in a web view.
+    // 웹 뷰 로딩이 시작 될때 호출되는 메서드
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        
+        self.spinner.startAnimating()
+    }
+    
+    // Called when a web view receives a server redirect.
+    // 웹 뷰 탐색이 완료되면 호출되는 메소드
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        
+        self.spinner.stopAnimating()
+    }
+    
+    // Called when an error occurs while the web view is loading content.
+    // 웹 뷰 로딩에 실패했을 때 호출되는 메소드
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        
+        // 인디케이트 뷰 애니메이션 중지
+        self.spinner.stopAnimating()
+        
+        // 알림
+        let alert = UIAlertController(title: "오류", message: "상세페이지를 읽어오지 못했습니다.", preferredStyle: .alert)
+        
+        let cancelBtn = UIAlertAction(title: "확인", style: .cancel){
+            (_) in
+            
+            // 이전 화면으로
+            _ = self.navigationController?.popViewController(animated: false)
+        }
+        
+        alert.addAction(cancelBtn)
+        self.present(alert, animated: false)
+        
+    }
+    
     override func viewDidLoad() {
+        
+        self.wv.navigationDelegate = self
         
         // 넘어온 객체 확인
         NSLog("linkurl = \(self.mvo?.detail), title = \(self.mvo?.title)")
